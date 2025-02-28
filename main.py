b6e34567aca94ce6ddff2ef51c8b4b1329131ab5
Patrick Byrnes <patrick.byrnes@outlook.com>
2025-02-28 14:04:09 -0600

    fix num hexagon calculations and initially convert measurements to inches

--- main.py
+++ main.py
@@ -13,30 +13,30 @@ class Hexagon:
         self.coordinate_center_y = side_length * 3/2 * center_y
         self.side_length = side_length
 
-        # convert from millimeters to inches for matplotlib
-        self.coordinate_center_x /= 25.4
-        self.coordinate_center_y /= 25.4
-        self.side_length /= 25.4
+        # # convert from millimeters to inches for matplotlib
+        # self.coordinate_center_x /= 25.4
+        # self.coordinate_center_y /= 25.4
+        # self.side_length /= 25.4
 
     def draw_hexagon(self, axis):
         hexagon = RegularPolygon((self.coordinate_center_x, self.coordinate_center_y), numVertices=6, radius=self.side_length, edgecolor='Blue', fill=False)
         axis.add_patch(hexagon)
 
 
-triangle_side_length = 2.5  # in millimeters
+triangle_side_length = 2.5 / 25.4  # in inches (to support matplotlib)
 
-overall_width = 100  # 10 centimeters in millimeters
-overall_height = 100  # 10 centimeters in millimeters
+overall_width = 10 / 2.54  # in inches
+overall_height = 10 / 2.54  # in inches
 
-num_hexagons_wide = 3 + math.ceil(overall_width / (2 * math.sqrt(3)*triangle_side_length))
-num_hexagons_tall = 3 + math.ceil(overall_height / (2 * math.sqrt(3)*triangle_side_length))
+num_hexagons_wide = 1 + math.ceil(overall_width / (2 * math.sqrt(3)*triangle_side_length))
+num_hexagons_tall = 1 + math.ceil(overall_height / (3 * triangle_side_length))
 
 hexagon_grid = [[Hexagon(idx, jdx, 2*triangle_side_length) for jdx in range(num_hexagons_tall)] for idx in range(num_hexagons_wide)]
 
-plt.rcParams['figure.figsize'] = overall_width/25.4, overall_height/25.4
+plt.rcParams['figure.figsize'] = overall_width, overall_height
 ax = plt.gca()
-ax.set_xlim([0, overall_width/25.4])
-ax.set_ylim([0, overall_height/25.4])
+ax.set_xlim([0, overall_width])
+ax.set_ylim([0, overall_height])
 
 for idx in range(num_hexagons_wide):
     for jdx in range(num_hexagons_tall):
